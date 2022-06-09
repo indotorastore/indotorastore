632c7dbe9991c75edb659e9d9ddfb6572cd7acbb
indotorastore <93023815+indotorastore@users.noreply.github.com>
2022-06-09 10:04:10 +0700

    Create Spam-wa.py

---
+++ Spam-wa.py
@@ -0,0 +1,21 @@
+GNU nano 6.3        spam-wa.py
+import requests, os
+
+url_api_spam_wa= "https://aink-sanz.herokuapp.com>
+
+os.system("clear")
+os.system("figlet Spam-wa")
+print("[-] Creator :Diony")
+print("[-] Youtube : diony")
+nomor = input("\n[?] input nomor  : ")
+jumlah = input("[?] input jumlah : ")
+print()
+data = {
+"nomor":nomor
+}
+for free_tutorial in range(int(jumlah)):
+        sanz = requests.get(url_api_spam_wa, para>
+if "Berhasil ngab ! .. Subrek Yt FREE TUTORIAL." >
+        print("[+] Berhasil Ngab")
+else:
+        print("[!] Gagal Ngab")
